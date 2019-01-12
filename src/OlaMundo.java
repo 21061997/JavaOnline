0b0465fec6f7cbe5f83414b020e57b080867a376
yuritj.yuri <yuritj.yuri@gmail.com>
2019-01-12 18:55:52 -0200

    Alteração idade e nome

--- src/OlaMundo.java
+++ src/OlaMundo.java
@@ -2,7 +2,13 @@ public class OlaMundo {
 
     public static void main(String args[]){
 
-        System.out.println("Olá Mundo");
+        int idade = 18;
+        String nome = "Yuri Vaz";
+
+        System.out.println("Nome: "+ nome);
+        System.out.println("Idade: "+ idade);
+
+
     }
 
 
