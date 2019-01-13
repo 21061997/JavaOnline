720e67ba9f8eddd0d0d16bfc317c7fe8a3fe1cb4
yuritj.yuri <yuritj.yuri@gmail.com>
2019-01-13 15:58:52 -0200

    Continuação com operadores.

---
+++ src/OperadoresLogicos.java
@@ -0,0 +1,10 @@
+public class OperadoresLogicos {
+
+    public static void main(String[] args) {
+        int idade = 18;
+        float salario = 1000f;
+
+        System.out.print(idade >= 18 || salario >= 3000);
+
+    }
+}
