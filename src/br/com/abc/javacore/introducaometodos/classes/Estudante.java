0cc3436c2acff52c4425a3fa8b630e44eed6e19f
yuritj.yuri <yuritj.yuri@gmail.com>
2019-01-26 10:34:28 -0200

    Exercicio de média notas de alunno.

--- src/br/com/abc/javacore/introducaometodos/classes/Estudante.java
+++ src/br/com/abc/javacore/introducaometodos/classes/Estudante.java
@@ -5,25 +5,29 @@ import java.text.DecimalFormat;
 public class Estudante {
     public String nome;
     public int idade;
-    public double[] notas = new double[3];
+    public double[] notas;
 
     DecimalFormat decimalFormat = new DecimalFormat("0.0");
 
-
     public void mediAluno(double notas[]) {
         this.notas = notas;
+        if (this.notas == null) {
+            System.out.println("O aluno" + this.nome + "não possuí notas cadastradas no sistema");
+            return; // Para sair do metódo
+        }
+
         double soma = 0;
-        for (int i = 0; i < notas.length; i++) {
-            soma += notas[i];
+        for (double notass : notas) {
+            soma += notass;
         }
 
         double mediaAluno = soma / 3;
         if (mediaAluno < 6.0) {
             System.out.println("O aluno," + this.nome + "de idade, " + this.idade + " teve sua média " + decimalFormat.format(mediaAluno) + " portanto esta reprovado");
-        }else{
-            System.out.println("O aluno," + this.nome + "de idade, " + this.idade + " teve sua média " + decimalFormat.format(mediaAluno)  + " portanto esta aprovado");
+        } else {
+            System.out.println("O aluno," + this.nome + "de idade, " + this.idade + " teve sua média " + decimalFormat.format(mediaAluno) + " portanto esta aprovado");
         }
-
+        System.out.println("-----------------------------------------------------------------------------------------------------------------------------------");
 
     }
 
--- src/br/com/abc/javacore/introducaometodos/test/EstudentTest.java
+++ src/br/com/abc/javacore/introducaometodos/test/EstudentTest.java
@@ -5,14 +5,23 @@ import br.com.abc.javacore.introducaometodos.classes.*;
 public class EstudentTest {
     public static void main(String[] args) {
         Estudante estudante = new Estudante();
-
-        double[] notas = new double[3];
-        notas[0] = 9.5;
-        notas[1] = 5.5;
-        notas[2] = 8.5;
+        estudante.notas = new double[]{9.5,5.5,8.5};
         estudante.nome = "Yuri Vaz Dos Santos";
         estudante.idade = 21;
-        estudante.mediAluno(notas);
+        estudante.mediAluno(estudante.notas);
+
+        Estudante estudante1 = new Estudante();
+        estudante1.nome = "Jeovana de Castro Silva";
+        estudante1.idade = 23;
+        //estudante1.notas = new double[]{4.5,6.7,7.8};
+        estudante1.mediAluno(estudante1.notas);
+
+        Estudante estudante2 = new Estudante();
+        estudante1.nome = "Milena dos Santos Peres";
+        estudante1.idade = 22;
+        estudante1.notas = new double[]{3.5,4.7,2.8};
+        estudante1.mediAluno(estudante1.notas);
+
 
 
     }
